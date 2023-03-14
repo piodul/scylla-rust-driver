790f795e0986cdb0a4d178b1eb550ab7babe71d1
Piotr Dulikowski <piodul@scylladb.com>
2023-03-14 12:02:03 +0100

    connection: migrate query_iter to new interface

--- scylla/src/transport/connection.rs
+++ scylla/src/transport/connection.rs
@@ -32,7 +32,7 @@ use std::{
 };
 
 use super::errors::{BadKeyspaceName, DbError, QueryError};
-use super::iterator::{LegacyRowIterator, RawIterator};
+use super::iterator::RawIterator;
 use super::query_result::{QueryResult, SingleRowError};
 use super::session::AddressTranslator;
 use super::topology::{PeerEndpoint, UntranslatedEndpoint, UntranslatedPeer};
@@ -580,7 +580,7 @@ impl Connection {
         self: Arc<Self>,
         query: Query,
         values: impl ValueList,
-    ) -> Result<LegacyRowIterator, QueryError> {
+    ) -> Result<RawIterator, QueryError> {
         let serialized_values = values.serialized()?.into_owned();
 
         let consistency = query
@@ -596,7 +596,6 @@ impl Connection {
             serial_consistency,
         )
         .await
-        .map(RawIterator::into_legacy)
     }
 
     #[allow(dead_code)]
@@ -1578,6 +1577,7 @@ mod tests {
     use scylla_cql::frame::protocol_features::{
         LWT_OPTIMIZATION_META_BIT_MASK_KEY, SCYLLA_LWT_ADD_METADATA_MARK_EXTENSION,
     };
+    use scylla_cql::frame::response::result::Row;
     use scylla_cql::frame::types;
     use scylla_proxy::{
         Condition, Node, Proxy, Reaction, RequestFrame, RequestOpcode, RequestReaction,
@@ -1671,6 +1671,7 @@ mod tests {
             .query_iter(select_query.clone(), &[])
             .await
             .unwrap()
+            .into_typed::<Row>()
             .try_collect::<Vec<_>>()
             .await
             .unwrap();
@@ -1704,6 +1705,7 @@ mod tests {
             .query_iter(insert_query, (0,))
             .await
             .unwrap()
+            .into_typed::<Row>()
             .try_collect::<Vec<_>>()
             .await
             .unwrap();
