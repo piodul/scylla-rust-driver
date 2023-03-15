78fc73154dd3ae8ba70f82bd04205ef597db0cc2
Piotr Dulikowski <piodul@scylladb.com>
2023-03-15 08:51:00 +0100

    scylla-cql/types: introduce new deserialization traits
    This commit introduces types and traits that will serve as the base of
    the new deserialization framework. It is meant to replace the old way of
    deserialization based on CqlValue, FromCqlVal and FromRow.
    
    The new traits are `DeserializeCql<'f>` and `DeserializeRow<'f>`. If a
    type implements `DeserializeCql<'s>`, this means that it can take a
    buffer of lifetime `'f` and deserialize itself, treating the contents of
    the buffer as a CQL value. Analogously, `DeserializeRow<'f>` allows for
    deserialization of types that are supposed to represent a whole row
    returned in the results.
    
    Deserialization is now split into two phases: type checking and actual
    deserialization. Both traits have two methods: `type_check` and
    `deserialize`. The idea behind this is to validate the column types of
    the response only once, after the response is received. The
    `deserialize` method is then called for each row/value to perform the
    actual parsing: that method is allowed to assume that `type_check` was
    called and may skip some type checking for performance (although it
    cannot use that assumption to perform unsafe operations, it is not an
    unsafe method).
    
    The previous framework only supported deserialization of types that
    exclusively own their data, for example String or Vec<u8>. However, the
    new framework allows for types that borrow or co-own the frame buffer:
    
    - Types that borrow the buffer are &'f str and &'f [u8]. They just point
      to the raw data from the serialized response. Rust's lifetime system
      makes sure that the the user doesn't deallocate the serialized
      response until using the deserialized types that point to the buffer
      are dropped.
      Apart from the aforementioned types, a bunch of iterator types have
      been introduced that allow consuming collections without allocations.
    - Serialized frame is represented with bytes::Bytes. It is possible to
      create a subslice (also of type Bytes) that keep the original Bytes
      alive. The type being deserialized can obtain access to such a
      subslice. Like in the case of e.g. &str vs. String, keeping Bytes also
      allows to avoid an allocation, but it is also easier to handle as the
      deserialized type doesn't have to be bound by any lifetime. It is
      important to be careful when handling such types as they keep the
      whole serialized frame alive, even if they only point to a small
      subslice. This can lead to a space leak and more memory being used
      than necessary.
    
    Care was taken to implement the new traits for all the types that were
    previously deserializable with the old framework.
    
    The commit also comes with a suite of tests - including a compatibility
    test which verifies that the old and new way of deserialization give the
    same results for all types that support both frameworks.
    
    This commit does _not_ include derive macros for the new traits. Those
    macros will be added in a later commit.

---
+++ scylla-cql/src/types/deserialize/mod.rs
@@ -0,0 +1,305 @@
+//! Framework for deserialization of data returned by database queries.
+//! 
+//! Deserialization is based on two traits:
+//! 
+//! - A type that implements `DeserializeCql<'frame>` can be deserialized
+//!   from a single _CQL value_ - i.e. an element of a row in the query result,
+//! - A type that implements `DeserializeRow<'frame>` can be deserialized
+//!   from a single _row_ of a query result.
+//! 
+//! Those traits are quite similar to each other, both in the idea behind them
+//! and the interface that they expose.
+//! 
+//! # `type_check` and `deserialize`
+//! 
+//! The deserialization process is divided into two parts: type checking and
+//! actual deserialization, represented by `DeserializeCql`/`DeserializeRow`'s
+//! methods called `type_check` and `deserialize`.
+//! 
+//! The `deserialize` method can assume that `type_check` was called before, so
+//! it doesn't have to verify the type again. This can be a performance gain
+//! when deserializing query results with multiple rows: as each row in a result
+//! has the same type, it is only necessary to call `type_check` once for the
+//! whole result and then `deserialize` for each row.
+//! 
+//! Note that `deserialize` is not an `unsafe` method - although you can be
+//! sure that the driver will call `type_check` before `deserialize`, you
+//! shouldn't do unsafe things based on this assumption.
+//! 
+//! # Data ownership
+//! 
+//! Some CQL types can be easily consumed while still partially serialized.
+//! For example, types like `blob` or `text` can be just represented with
+//! `&[u8]` and `&str` that just point to a part of the serialized response.
+//! This is more efficient than using `Vec<u8>` or `String` because it avoids
+//! an allocation and a copy, however it is less convenient because those types
+//! are bound with a lifetime.
+//! 
+//! The framework supports types that refer to the serialized response's memory
+//! in three different ways:
+//! 
+//! ## Owned types
+//! 
+//! Some types don't borrow anything and fully own their data, e.g. `i32` or
+//! `String`. They aren't constrained by any lifetime and should implement
+//! the respective trait for _all_ lifetimes, i.e.:
+//! 
+//! ```rust
+//! # use crate::frame::types::deserialize::row::DeserializeValue;
+//! # use crate::frame::response::result::ColumnType;
+//! # use crate::frame::frame_errors::ParseError;
+//! impl<'frame> DeserializeValue<'frame> for Vec<u8> {
+//!     fn type_check(typ: &ColumnType) -> Result<(), ParseError> {
+//!          if let CqlType::Bytes = typ {
+//!              return Ok(());
+//!          }
+//!          Err(ParseError::BadIncomingData("Expected bytes".to_string()))
+//!      }
+//! 
+//!      fn deserialize(
+//!          _typ: &'frame ColumnType,
+//!          v: Option<FrameSlice<'frame>>,
+//!      ) -> Result<Self, ParseError> {
+//!          v
+//!             .map(|s| s.to_vec())
+//!             .ok_or_else(|| {
+//!                 ParseError::BadIncomingData("Expected non-null value".to_string())
+//!             })
+//!      }
+//! }
+//! ```
+//! 
+//! ## Borrowing types
+//! 
+//! Some types do not fully contain their data but rather will point to some
+//! bytes in the serialized response, e.g. `&str` or `&[u8]`. Those types will
+//! usually contain a lifetime in their definition. In order to properly
+//! implement `DeserializeCql` or `DeserializeRow` for such a type, the `impl`
+//! should still have a generic lifetime parameter, but the lifetimes from the
+//! type definition should be constrained with the generic lifetime parameter.
+//! For example:
+//! 
+//! ```rust
+//! # use crate::frame::types::deserialize::row::DeserializeValue;
+//! # use crate::frame::response::result::ColumnType;
+//! # use crate::frame::frame_errors::ParseError;
+//! impl<'a, 'frame> DeserializeValue<'frame> for &'a [u8]
+//! where
+//!     'frame: 'a,
+//! {
+//!     fn type_check(typ: &ColumnType) -> Result<(), ParseError> {
+//!          if let CqlType::Bytes = typ {
+//!              return Ok(());
+//!          }
+//!          Err(ParseError::BadIncomingData("Expected bytes".to_string()))
+//!      }
+//! 
+//!      fn deserialize(
+//!          _typ: &'frame ColumnType,
+//!          v: Option<FrameSlice<'frame>>,
+//!      ) -> Result<Self, ParseError> {
+//!          v.ok_or_else(|| {
+//!             ParseError::BadIncomingData("Expected non-null value".to_string())
+//!          })
+//!      }
+//! }
+//! ```
+//! 
+//! ## Reference-counted types (`DeserializeCql` only)
+//! 
+//! Internally, the driver uses the `bytes::Bytes` type to keep the contents
+//! of the serialized response. It supports creating derived `Bytes` objects
+//! which point to a subslice but keep the whole, original `Bytes` object alive.
+//! 
+//! During deserialization, a type can obtain a `Bytes` subslice that point
+//! to the serialized value. This approach combined advantages of the previous
+//! two approaches - creating a derived `Bytes` object can be cheaper than
+//! allocation and a copy (it supports `Arc`-like semantics) and the `Bytes`
+//! type is not constrained by a lifetime. However, you should be aware that
+//! the subslice will keep the whole `Bytes` object that holds the frame alive.
+//! It is not recommended to use this approach for long-living objects because
+//! it can introduce space leaks.
+//! 
+//! Example:
+//! 
+//! ```rust
+//! # use crate::frame::types::deserialize::row::DeserializeValue;
+//! # use crate::frame::response::result::ColumnType;
+//! # use crate::frame::frame_errors::ParseError;
+//! # use bytes::Bytes;
+//! impl<'frame> DeserializeValue<'frame> for Bytes {
+//!     fn type_check(typ: &ColumnType) -> Result<(), ParseError> {
+//!          if let CqlType::Bytes = typ {
+//!              return Ok(());
+//!          }
+//!          Err(ParseError::BadIncomingData("Expected bytes".to_string()))
+//!      }
+//! 
+//!      fn deserialize(
+//!          _typ: &'frame ColumnType,
+//!          v: Option<FrameSlice<'frame>>,
+//!      ) -> Result<Self, ParseError> {
+//!          v
+//!             .map(|s| s.to_bytes())
+//!             .ok_or_else(|| {
+//!                 ParseError::BadIncomingData("Expected non-null value".to_string())
+//!             })
+//!      }
+//! }
+//! ```
+
+pub mod row;
+pub mod value;
+
+use bytes::Bytes;
+
+use crate::frame::frame_errors::ParseError;
+use crate::frame::types;
+
+/// A reference to a part of the frame.
+#[derive(Clone, Copy, Debug)]
+pub struct FrameSlice<'frame> {
+    mem: &'frame [u8],
+    frame: &'frame Bytes,
+}
+
+static EMPTY_BYTES: Bytes = Bytes::new();
+
+impl<'frame> FrameSlice<'frame> {
+    /// Creates a new FrameSlice from a reference of a Bytes object.
+    ///
+    /// This method is exposed to allow writing deserialization tests
+    /// for custom types.
+    #[inline]
+    pub fn new(frame: &'frame Bytes) -> Self {
+        Self { mem: frame, frame }
+    }
+
+    /// Creates a new FrameSlice that refers to a subslice of a given Bytes object.
+    #[inline]
+    pub fn new_subslice(mem: &'frame [u8], frame: &'frame Bytes) -> Self {
+        Self { mem, frame }
+    }
+
+    /// Creates an empty FrameSlice.
+    #[inline]
+    pub fn new_empty() -> Self {
+        Self {
+            mem: &EMPTY_BYTES,
+            frame: &EMPTY_BYTES,
+        }
+    }
+
+    /// Returns a reference to the slice.
+    #[inline]
+    pub fn as_slice(&self) -> &'frame [u8] {
+        self.mem
+    }
+
+    /// Returns `true` if the slice has length of 0.
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.mem.is_empty()
+    }
+
+    /// Returns a reference to the Bytes object which encompasses the slice.
+    ///
+    /// The Bytes object will usually be larger than the slice returned by
+    /// [FrameSlice::as_slice]. If you wish to obtain a new Bytes object that
+    /// points only to the subslice represented by the FrameSlice object,
+    /// see [FrameSlice::to_bytes].
+    #[inline]
+    pub fn as_bytes_ref(&self) -> &'frame Bytes {
+        self.frame
+    }
+
+    /// Returns a new Bytes object which is a subslice of the original slice
+    /// object.
+    #[inline]
+    pub fn to_bytes(&self) -> Bytes {
+        self.frame.slice_ref(self.mem)
+    }
+
+    /// Reads and consumes a `[bytes]` item from the beginning of the frame.
+    ///
+    /// If the operation fails then the slice remains unchanged.
+    #[inline]
+    fn read_cql_bytes(&mut self) -> Result<Option<FrameSlice<'frame>>, ParseError> {
+        match types::read_bytes_opt(&mut self.mem) {
+            Ok(Some(slice)) => Ok(Some(Self::new_subslice(slice, self.frame))),
+            Ok(None) => Ok(None),
+            Err(err) => Err(err),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::frame::types;
+
+    use super::*;
+
+    use bytes::{Bytes, BytesMut};
+
+    static CELL1: &[u8] = &[1, 2, 3];
+    static CELL2: &[u8] = &[4, 5, 6, 7];
+
+    pub(super) fn serialize_cells(
+        cells: impl IntoIterator<Item = Option<impl AsRef<[u8]>>>,
+    ) -> Bytes {
+        let mut bytes = BytesMut::new();
+        for cell in cells {
+            types::write_bytes_opt(cell, &mut bytes).unwrap();
+        }
+        bytes.freeze()
+    }
+
+    #[test]
+    fn test_cql_bytes_consumption() {
+        let frame = serialize_cells([Some(CELL1), None, Some(CELL2)]);
+        let mut slice = FrameSlice::new(&frame);
+        assert!(!slice.is_empty());
+
+        assert_eq!(
+            slice.read_cql_bytes().unwrap().map(|s| s.as_slice()),
+            Some(CELL1)
+        );
+        assert!(!slice.is_empty());
+        assert!(slice.read_cql_bytes().unwrap().is_none());
+        assert!(!slice.is_empty());
+        assert_eq!(
+            slice.read_cql_bytes().unwrap().map(|s| s.as_slice()),
+            Some(CELL2)
+        );
+        assert!(slice.is_empty());
+        slice.read_cql_bytes().unwrap_err();
+        assert!(slice.is_empty());
+    }
+
+    #[test]
+    fn test_cql_bytes_owned() {
+        let frame = serialize_cells([Some(CELL1), Some(CELL2)]);
+        let mut slice = FrameSlice::new(&frame);
+
+        let subslice1 = slice.read_cql_bytes().unwrap().unwrap();
+        let subslice2 = slice.read_cql_bytes().unwrap().unwrap();
+
+        assert_eq!(subslice1.as_slice(), CELL1);
+        assert_eq!(subslice2.as_slice(), CELL2);
+
+        assert_eq!(
+            subslice1.as_bytes_ref() as *const Bytes,
+            &frame as *const Bytes
+        );
+        assert_eq!(
+            subslice2.as_bytes_ref() as *const Bytes,
+            &frame as *const Bytes
+        );
+
+        let subslice1_bytes = subslice1.to_bytes();
+        let subslice2_bytes = subslice2.to_bytes();
+
+        assert_eq!(subslice1.as_slice(), subslice1_bytes.as_ref());
+        assert_eq!(subslice2.as_slice(), subslice2_bytes.as_ref());
+    }
+}
